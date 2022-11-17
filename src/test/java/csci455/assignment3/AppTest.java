2682fc3588a2ead15c35eef4db044c1e9a245f71
Zak Ray Merrigan <zak.merrigan@ndsu.edu>
2022-11-16 20:19:29 -0600

    Established Unit Testing

--- src/test/java/csci455/assignment3/AppTest.java
+++ src/test/java/csci455/assignment3/AppTest.java
@@ -1,4 +1,11 @@
 package csci455.assignment3;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 public class AppTest {
-    
+    @Test
+    public void testMethod1()
+    {
+        String test = "Testing Environment";
+        Assertions.assertEquals("Testing Environment", test);
+    }
 }
