75a44da6b3434be305c03959c5aae6761bceb5af
jayk73 <jayk7317@gmail.com>
2022-11-26 16:56:13 -0600

    updated some comments

--- src/main/java/csci455/assignment3/App.java
+++ src/main/java/csci455/assignment3/App.java
@@ -17,10 +17,11 @@ public class App
         private Text word = new Text();
         
         //parses input one line at a time
+        //need to change this to parse and store peers in some structure
         public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
             String line = value.toString();
             StringTokenizer tokenizer = new StringTokenizer(line);
-            while (tokenizer.hasMoreTokens()) { //one line
+            while (tokenizer.hasMoreTokens()) { //one line per loop
                 word.set(tokenizer.nextToken());
                 output.collect(word, one);
             }
@@ -29,7 +30,7 @@ public class App
     
     public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
         
-        //right now this adds up the number of words
+        //right now this adds up the number of each word
         //in final implemetation, this will identify common peers of two nodes
         public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
             int sum = 0;
@@ -48,16 +49,16 @@ public class App
     conf.setOutputKeyClass(Text.class);
     conf.setOutputValueClass(IntWritable.class);
 
-    conf.setMapperClass(Map.class);
-    conf.setCombinerClass(Reduce.class);
-    conf.setReducerClass(Reduce.class);
+    conf.setMapperClass(Map.class);//splits lines into tokens
+    conf.setCombinerClass(Reduce.class);//combines tokens 
+    conf.setReducerClass(Reduce.class);//finds common pairs
 
-    conf.setInputFormat(TextInputFormat.class);
-    conf.setOutputFormat(TextOutputFormat.class);
+    conf.setInputFormat(TextInputFormat.class);//sets up input
+    conf.setOutputFormat(TextOutputFormat.class);//sets up output
 
     FileInputFormat.setInputPaths(conf, new Path(args[0]));
     FileOutputFormat.setOutputPath(conf, new Path(args[1]));
 
-    JobClient.runJob(conf);
+    JobClient.runJob(conf);//runs the job
     }
 }
