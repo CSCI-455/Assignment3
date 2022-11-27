cf620cc7c9cf4cbca3d7ac40007c3901f60451bf
jayk73 <jayk7317@gmail.com>
2022-11-27 11:31:00 -0600

    changed wordcount programs

--- src/main/java/csci455/assignment3/App.java
+++ src/main/java/csci455/assignment3/App.java
@@ -1,64 +1,67 @@
 package csci455.assignment3;
 
 import java.io.IOException;
-import java.util.*;
-
+import java.util.StringTokenizer;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.conf.*;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapred.*;
-import org.apache.hadoop.util.*;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 
 public class App
 {
     
-    public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+    public static class Map extends Mapper<Object, Text, Text, IntWritable>{
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
-        
-        //parses input one line at a time
-        //need to change this to parse and store peers in some structure
-        public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
-            String line = value.toString();
-            StringTokenizer tokenizer = new StringTokenizer(line);
-            while (tokenizer.hasMoreTokens()) { //one line per loop
-                word.set(tokenizer.nextToken());
-                output.collect(word, one);
+
+            //parses input one line at a time
+            //need to change this to parse and store peers in some structure
+            public void map(Object key, Text value, Context context
+            ) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+            word.set(itr.nextToken());
+            context.write(word, one);
             }
         }
     }
+
+    public static class IntSumReducer
+    extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    //right now this adds up the number of each word
+    //in final implemetation, this will identify common peers of two nodes
+    public void reduce(Text key, Iterable<IntWritable> values,Context context) throws IOException, InterruptedException {
+        int sum = 0;
+        for (IntWritable val : values) {
+            sum += val.get();
+        }
+        result.set(sum);
+        context.write(key, result);
+        }
+    }
+
     
-    public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
-        
-        //right now this adds up the number of each word
-        //in final implemetation, this will identify common peers of two nodes
-        public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
-            int sum = 0;
-            while (values.hasNext()) {
-                sum += values.next().get();
-            }
-            output.collect(key, new IntWritable(sum));
-        }
+    public static void main(String[] args) throws Exception {
+        //sets up job configuration for hadoop
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "word count");
+    job.setJarByClass(App.class);
+    job.setMapperClass(Map.class);//splits lines into tokens
+    job.setCombinerClass(IntSumReducer.class);//combines tokens
+    job.setReducerClass(IntSumReducer.class);//finds common pairs
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
-    public static void main(String[] args) throws Exception
-    {
-    //sets up job configuration for hadoop
-    JobConf conf = new JobConf(App.class);
-    conf.setJobName("wordcount");
 
-    conf.setOutputKeyClass(Text.class);
-    conf.setOutputValueClass(IntWritable.class);
-
-    conf.setMapperClass(Map.class);//splits lines into tokens
-    conf.setCombinerClass(Reduce.class);//combines tokens 
-    conf.setReducerClass(Reduce.class);//finds common pairs
-
-    conf.setInputFormat(TextInputFormat.class);//sets up input
-    conf.setOutputFormat(TextOutputFormat.class);//sets up output
-
-    FileInputFormat.setInputPaths(conf, new Path(args[0]));
-    FileOutputFormat.setOutputPath(conf, new Path(args[1]));
-
-    JobClient.runJob(conf);//runs the job
-    }
 }
